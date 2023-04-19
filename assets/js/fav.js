c43bf1122f7519521815d076706171d5f1dfc2fb
Jhelt <jonhelton405@gmail.com>
2023-04-19 02:02:11 -0400

    favorites page styling update and functionality

--- assets/js/fav.js
+++ assets/js/fav.js
@@ -1,25 +1,75 @@
-
 let favorites = JSON.parse(localStorage.getItem('favoritesDrinks'));
-console.log(favorites);
+let favIngredients = JSON.parse(localStorage.getItem('favoriteIngredients'))
+let favMethod = JSON.parse(localStorage.getItem('favoriteInstructions'))
+let favImage = JSON.parse(localStorage.getItem('favoritesThumbnail'))
 
-let ulEl = document.createElement("ul")
-for (let i = 0; i < favorites.length; i++) {
-  let liEl = document.createElement("li");
-  liEl.textContent = favorites[i];
-  liEl.textContent = favorites[i];
-  liEl.style.borderBottom = "1px solid black";
-  liEl.style.position = "relative";
-  liEl.style.top = "-7rem";
-  liEl.style.left = "-15em";
-  liEl.style.margin = "0";
-  liEl.style.color = "#B6CBDD";
-  liEl.style.fontSize = "16px";
-  liEl.style.display = "flex";
-  liEl.style.justifyContent = "center" 
-  
-  ulEl.appendChild(liEl);
+var showCase = document.querySelector(".favShowcase")
+let saveBtn = document.querySelector(".fav")
+
+var faveIndex
+
+let Menu = document.createElement("div")
+
+function makeFaveArray() {
+  for (let i = 0; i < favorites.length; i++) {
+    let buttonMenu = document.createElement("button");
+    buttonMenu.setAttribute("class", "buttons")
+    buttonMenu.textContent = favorites[i];
+    
+    Menu.appendChild(buttonMenu);
+    
+    buttonMenu.addEventListener("click", function () {
+      displayFavorite(i);
+    });
+  }
 }
-ulEl.setAttribute("class", "favoritedDrinks");
 
-let favoritedDrink = document.getElementById("favoritedDrink");
-favoritedDrink.appendChild(ulEl)
+
+function displayFavorite(i) {
+  showCase.innerHTML = "";
+  let favName = document.createElement("h1")
+
+  let favImageDisplay = document.createElement("img")
+  favImageDisplay.setAttribute("class", "displayedImage")
+
+  let favIngrDisplay = document.createElement("ul")
+  favIngrDisplay.setAttribute("class", "displayedList")
+  for (let j = 0; j < favIngredients[i].length; j++) {
+    let listItem = document.createElement("li")
+    listItem.textContent = favIngredients[i][j]
+    favIngrDisplay.appendChild(listItem)
+  }  
+
+  let favDescription = document.createElement("p")
+  favDescription.setAttribute("class", "itemDescription")
+
+  favName.textContent = favorites[i]
+
+  favImageDisplay.setAttribute("src", favImage[i])
+
+  if (favMethod[i]) {
+    favDescription.textContent = favMethod[i]
+  }
+  
+  showCase.appendChild(favName)
+  showCase.appendChild(favImageDisplay)
+  showCase.appendChild(favIngrDisplay)
+  showCase.appendChild(favDescription)
+}
+
+makeFaveArray()
+
+Menu.setAttribute("class", "menuDrinks");
+
+let favoritedDrink = document.querySelector(".favoritedDrink");
+favoritedDrink.appendChild(Menu)
+
+saveBtn.addEventListener("click", function (event) {
+  let clickedElement = event.target;
+
+  // listen for button click
+  if (clickedElement.matches(".buttons")) {
+    let index = Array.from(clickedElement.parentNode.children).indexOf(clickedElement);
+    displayFavorite(index);
+  }
+})
